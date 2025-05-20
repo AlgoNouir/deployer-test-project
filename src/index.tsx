6e2dec18f84dbec089f8aa72bc08e9e0f957f9b0
algonouir <algo.mahdi.nouri@gmail.com>
2025-05-21 00:27:49 +0330

    first test

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,9 @@ function App() {
     <div class="flex flex-col items-center justify-center h-screen">
       <h1 class="text-4xl">Hello World</h1>
       <p>This is a test for testing automatic deployment</p>
+      <li>
+        <p>first test for `webhook` checking</p>
+      </li>
     </div>
   );
 }
