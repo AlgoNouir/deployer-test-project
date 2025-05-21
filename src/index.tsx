1287f9b47d533ec87f5360aae89c534db18d6bcf
algonouir <algo.mahdi.nouri@gmail.com>
2025-05-21 13:25:11 +0330

    test 3th

--- src/index.tsx
+++ src/index.tsx
@@ -4,23 +4,7 @@ import "./index.css";
 function App() {
   return (
     <div class="flex flex-col items-center justify-center h-screen">
-      <h1 class="text-4xl">Hello World</h1>
-      <p>This is a test for testing automatic deployment</p>
-      <li>
-        <p>first test for `webhook` checking</p>
-      </li>
-      <li>
-        <p>perfect. i can see it now. but just with simple request</p>
-      </li>
-      <li>
-        <p>now i want to test with a more complex request. test on port 9000</p>
-      </li>
-      <li>
-        <p>
-          perfect. i can see it now. but just with simple request. i want to
-          test with a github hook ...
-        </p>
-      </li>
+      <h1>THIS IS WORKS</h1>
     </div>
   );
 }
